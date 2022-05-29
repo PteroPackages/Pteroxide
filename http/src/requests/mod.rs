6ba438f7a816ec6b4ddde19df47df20d31906d81
devnote-dev <inferno0745@gmail.com>
2022-05-30 00:59:34 +0100

    feat(http): reimplement setter methods

--- http/src/requests/mod.rs
+++ http/src/requests/mod.rs
@@ -1,4 +1,7 @@
-use hyper::Method;
+use hyper::{Body, Method};
+use serde_json::Value;
+
+use crate::errors::Error;
 
 pub mod account;
 
@@ -6,13 +9,13 @@ pub mod account;
 pub struct RequestBuilder {
     pub method: Method,
     pub path: String,
-    pub body: Vec<u8>,
+    pub body: Body,
 }
 
 impl Default for RequestBuilder {
     fn default() -> Self {
         Self {
-            method: Method::GET,
+            method: Default::default(),
             path: Default::default(),
             body: Default::default(),
         }
@@ -21,8 +24,25 @@ impl Default for RequestBuilder {
 
 impl RequestBuilder {
     pub fn new(path: &str) -> Self {
-        let mut r = Self::default();
-        r.path = String::from(path);
-        r
+        let mut b = Self::default();
+        b.path = String::from(path);
+
+        b
+    }
+
+    pub fn json(&mut self, data: Value) {
+        let v = data.as_str().unwrap();
+        self.body = Body::from(v.to_owned());
+    }
+
+    pub fn text(&mut self, data: String) {
+        self.body = Body::from(data.as_bytes().to_owned());
+    }
+
+    pub fn method(&mut self, method: &str) -> Result<(), Error> {
+        match Method::try_from(method) {
+            Ok(m) => Ok(self.method = m),
+            Err(_) => Err(Error::from("invalid http method")),
+        }
     }
 }
