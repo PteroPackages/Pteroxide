20f4a5b4456083027f4ca8a304c9c6569da949d0
devnote-dev <inferno0745@gmail.com>
2022-11-22 23:58:46 +0000

    feat(models): separate fractal into feature mod

---
+++ pteroxide-models/src/fractal.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result as FmtResult},
+};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FeatureLimits {
+    allocations: u32,
+    backups: u32,
+    databases: u32,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct ErrorData {
+    pub code: String,
+    pub status: String,
+    pub detail: String,
+}
+
+impl Display for ErrorData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "code: {}; status: {}; detail: {}",
+            self.code, self.status, self.detail
+        )
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FractalError {
+    pub errors: Vec<ErrorData>,
+}
+
+impl Display for FractalError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        for e in self.errors.iter() {
+            match Display::fmt(&e, f) {
+                Ok(_) => (),
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Error for FractalError {}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FractalItem<T> {
+    pub object: String,
+    pub attributes: T,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FractalList<T> {
+    pub object: String,
+    pub data: Vec<FractalItem<T>>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Limits {
+    pub memory: u64,
+    pub swap: u64,
+    pub disk: u64,
+    pub io: Option<String>,
+    pub cpu: u64,
+    pub threads: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub oom_disabled: Option<bool>,
+}
