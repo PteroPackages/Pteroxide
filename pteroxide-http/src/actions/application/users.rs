b25d1ae2749dee7fb78ad5a9b27c7808360da691
devnote-dev <inferno0745@gmail.com>
2022-11-22 00:05:42 +0000

    feat(http): start implementing application actions

---
+++ pteroxide-http/src/actions/application/users.rs
@@ -0,0 +1,29 @@
+use crate::{
+    error::Error,
+    http::Http,
+    request::{Builder, routing::application::Route},
+};
+
+use pteroxide_models::{application::User, FractalList};
+
+#[derive(Debug)]
+pub struct GetUsers {
+    http: Http,
+}
+
+impl GetUsers {
+    pub const fn new(http: Http) -> Self {
+        Self { http }
+    }
+
+    pub async fn exec(self) -> Result<Vec<User>, Error> {
+        let res = self.http.request::<FractalList<User>>(
+            Builder::default().route(Route::ListUsers)
+        ).await?;
+
+        Ok(res.data
+            .iter()
+            .map(|u| u.attributes.clone())
+            .collect())
+    }
+}
--- pteroxide-http/src/request/mod.rs
+++ pteroxide-http/src/request/mod.rs
@@ -1,2 +1,4 @@
 pub mod builder;
 pub mod routing;
+
+pub use self::builder::Builder;
