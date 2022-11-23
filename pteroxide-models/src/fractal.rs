07e9a0fcb6eb1356e9a6531104ed7887c2f2c6a7
devnote-dev <inferno0745@gmail.com>
2022-11-23 18:23:56 +0000

    fix(models): move general structs out of fractal

--- pteroxide-models/src/fractal.rs
+++ pteroxide-models/src/fractal.rs
@@ -5,13 +5,6 @@ use std::{
 };
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct FeatureLimits {
-    allocations: u32,
-    backups: u32,
-    databases: u32,
-}
-
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct ErrorData {
     pub code: String,
     pub status: String,
@@ -58,16 +51,4 @@ pub struct FractalItem<T> {
 pub struct FractalList<T> {
     pub object: String,
     pub data: Vec<FractalItem<T>>,
-}
-
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct Limits {
-    pub memory: u64,
-    pub swap: u64,
-    pub disk: u64,
-    pub io: Option<String>,
-    pub cpu: u64,
-    pub threads: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub oom_disabled: Option<bool>,
 }
--- pteroxide-models/src/lib.rs
+++ pteroxide-models/src/lib.rs
@@ -1,5 +1,26 @@
+use serde::{Deserialize, Serialize};
+
 #[cfg(feature = "app")]
 pub mod application;
 
 #[cfg(feature = "fractal")]
 pub mod fractal;
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FeatureLimits {
+    allocations: u32,
+    backups: u32,
+    databases: u32,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Limits {
+    pub memory: u64,
+    pub swap: u64,
+    pub disk: u64,
+    pub io: Option<String>,
+    pub cpu: u64,
+    pub threads: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub oom_disabled: Option<bool>,
+}
