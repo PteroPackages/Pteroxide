807568aeccf62f318f7cdaf4e44ad7e91afdfaa2
devnote-dev <inferno0745@gmail.com>
2022-11-24 17:24:25 +0000

    feat(http/application): support including user servers

--- pteroxide-http/src/application/users.rs
+++ pteroxide-http/src/application/users.rs
@@ -5,17 +5,29 @@ use crate::{routing::application::Route, Application, Builder, Error};
 #[derive(Debug)]
 pub struct GetUsers<'a> {
     app: &'a Application,
+    with_servers: bool,
 }
 
 impl<'a> GetUsers<'a> {
     pub const fn new(app: &'a Application) -> Self {
-        Self { app }
+        Self { app, with_servers: false }
+    }
+
+    pub fn with_servers(mut self, value: bool) -> Self {
+        self.with_servers = value;
+
+        self
     }
 
     pub async fn exec(&self) -> Result<Vec<User>, Error> {
+        let mut builder = Builder::default().route(Route::GetUsers);
+        if self.with_servers {
+            builder = builder.param("include", "servers");
+        }
+
         let res = self
             .app
-            .request::<FractalList<User>>(Builder::default().route(Route::GetUsers))
+            .request::<FractalList<User>>(builder)
             .await?;
 
         Ok(res.data.iter().map(|u| u.attributes.clone()).collect())
